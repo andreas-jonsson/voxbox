5775cd6d96bd2d1fae31504e23d64a5188ffd6b6
Andreas T Jonsson <mail@andreasjonsson.se>
2016-12-06 20:48:53 +0100

    Added banner.

--- voxbox.go
+++ voxbox.go
@@ -1,14 +1,21 @@
-// +----------------=V=o=x=B=o=x=-=E=n=g=i=n=e=-----------------+
-// | Copyright (C) 2016 Andreas T Jonsson. All rights reserved. |
-// | Contact <mail@andreasjonsson.se>                           |
-// +------------------------------------------------------------+
-
 //go:generate go run data/generate.go
 
 package main
 
-import "github.com/andreas-jonsson/voxbox/entry"
+import (
+	"fmt"
+
+	"github.com/andreas-jonsson/voxbox/entry"
+)
+
+var banner = `
++----------------=V=o=x=B=o=x=-=E=n=g=i=n=e=-----------------+
+| Copyright (C) 2016 Andreas T Jonsson. All rights reserved. |
+| Contact <mail@andreasjonsson.se>                           |
++------------------------------------------------------------+
+`
 
 func main() {
+	fmt.Println(banner)
 	entry.Entry()
 }
